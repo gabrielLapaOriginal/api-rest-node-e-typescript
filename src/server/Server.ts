8cf9b821e90b5f11f4802c97e9e7ec06bbbf858a
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-05-07 12:05:32 -0300

    feat: added code style configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,9 +2,13 @@ import express from 'express';
 
 const server = express();
 
-server.get('/', (req, res)=>{ //req= requesicao para o sevidor, res= resposta
+interface Teste {
+  
+}
 
-    return res.send("Ola dev")
+server.get('/', (req, res)=>{ //req= requesicao para o sevidor, res= resposta
+  
+  return res.send("Ola dev")
 }) 
 
 export {  server };
