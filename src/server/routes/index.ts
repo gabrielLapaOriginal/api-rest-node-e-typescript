f6b5597b658af175a9689e638898781d96f8b708
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-05-19 11:44:59 -0300

    Changes on the path of files

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router  } from "express"; //cria um middleware onde especificamos todas as rotas, depois passando para o server
-import 'dotenv/config'
 import { StatusCodes } from 'http-status-codes'
+
+import { CidadesController } from './../controllers'
 
 
   const router = Router();
@@ -8,12 +9,9 @@ import { StatusCodes } from 'http-status-codes'
   router.get('/', (req, res)=>{ //req= requesicao para o sevidor, res= resposta
     return res.send("Ola dev")
   }) 
-  router.post(
-    '/teste', 
-    (req, res)=>{ //req= requesicao para o sevidor, res= resposta
-    console.log(req);
-    return res.status(StatusCodes.UNAUTHORIZED).json(req.body)
-  }) 
+
+
+  router.post('/cidades', CidadesController.create);
 
 
 
